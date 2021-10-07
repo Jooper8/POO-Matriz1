cab33ef3eff276525986df2b55b791e0ae3d9f0c
Frederico Proença <joopereight@gmail.com>
2021-10-06 21:48:14 -0300

    Colocados comentários.

--- POO-Matriz1/Program.cs
+++ POO-Matriz1/Program.cs
@@ -7,9 +7,13 @@ namespace POO_Matriz1
         static void Main(string[] args)
         {
             Console.WriteLine("Insert the order of the Square Matrix:");
+            //Inserção do usuário da ordem da matriz, que no caso desse código, sempre será quadrada.
             int n = int.Parse(Console.ReadLine());
+            //Declaração do array mat, que servirá como matriz.
             int[,] mat = new int[n, n];
-            Console.WriteLine("Now insert each value, by row (separate each number by ',' and press enter for each row:)");
+            Console.WriteLine("Now insert each integer value, by row (separate each number by ',' and press enter for each row:)");
+
+            //Inserção da matriz por coluna e separada por vírgula pelo usuário. A quantidade de linhas e colunas inseridas dependem do valor inserido anteriormente na linha 11.
             for (int i = 0; i<n; i++)
             {
                 string[] values = Console.ReadLine().Split(",");
@@ -18,6 +22,8 @@ namespace POO_Matriz1
                     mat[i, j] = int.Parse(values[j]);
                 }
             }
+
+            //Parte do código que encontra a diagonal principal da matriz, que é composto pelos valores de linha e coluna iguais.
             Console.WriteLine("Main diagonal:");
             for (int i = 0; i<n; i++)
             {
@@ -25,6 +31,7 @@ namespace POO_Matriz1
             }
             Console.WriteLine("");
 
+            //Contagem de números negativos encontrados na matriz.
             int count = 0;
             for (int i = 0; i < n; i++)
             {
@@ -36,6 +43,8 @@ namespace POO_Matriz1
                     }
                 }
             }
+
+            //Menção da quantidade de números negativos encontrados na matriz ao usuário.
             Console.WriteLine("Negative numbers: " + count);
         }
     }
